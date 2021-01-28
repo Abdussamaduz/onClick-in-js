00e9ea311a3155088d2033d0b19fd412ec608096
Abdussamaduz <kamoliddin135@gmail.com>
2021-01-28 07:29:13 +0500

    Update main.js

---
+++ main.js
@@ -0,0 +1,23 @@
+
+let button = document.querySelector('.bos')
+
+button.onclick = () => {
+    let body = document.querySelector('.body')
+    body.style.backgroundColor = '#f009'
+}
+
+
+let butto = document.querySelector('.bos2')
+
+butto.onclick = () => {
+    let body = document.querySelector('.body')
+    body.style.backgroundColor = '#ff09'
+}
+
+
+let butt = document.querySelector('.bos3')
+
+butt.onclick = () => {
+    let body = document.querySelector('.body')
+    body.style.backgroundColor = '#0f09'
+}
