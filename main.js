e3806035eaaa50f87065de4124eb588f59fb05a3
Abdussamaduz <kamoliddin135@gmail.com>
2021-01-31 11:37:53 +0500

    Update main.js

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ button.onclick = () => {
     const a = document.querySelector(".box")
     const txt = document.querySelector(".h1")
     body.style.backgroundColor = "#f009";
+    body.style.transition = "0.4s";
     a.style.borderColor = "#0f0"
     txt.style.color = "#0f0"
 };
@@ -17,6 +18,7 @@ butto.onclick = () => {
     const txt = document.querySelector(".h1")
     const a = document.querySelector(".box")
     bod.style.backgroundColor = "#ff09";
+    bod.style.transition = "0.7s";
     a.style.borderColor = "#000"
     txt.style.color = "#000"
 };
@@ -29,6 +31,7 @@ butt.onclick = () => {
     const ao = document.querySelector(".box")
     const txt = document.querySelector(".h1")
     bo.style.backgroundColor = "#0f09";
+    bo.style.transition = "0.7s";
     ao.style.borderColor = "#f00"
     txt.style.color = "#f00"
 };
@@ -40,6 +43,9 @@ but.onclick = () => {
     const a = document.querySelector(".box")
     const txt = document.querySelector(".h1")
     a.style.borderColor = "#ff0"
+    a.style.transition = "0.4s";
     b.style.backgroundColor = "#000e";
+    b.style.transition = "0.7s";
     txt.style.color = "#ff0"
+    txt.style.transition = "0.4s";
 };
